ec972f90b4765ea83c2ae6e3b40a3b8356fcd2da
Tuna Turkmen <tunaturkmn@gmail.com>
2021-07-19 20:57:15 +0100

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean hungry = true;
+		
+		if (hungry == true) {
+			System.out.println("Eat Something");
+		}
+		else {
+			System.out.println("Do your work!");
+		}
+		
+
+	}
+
+}
